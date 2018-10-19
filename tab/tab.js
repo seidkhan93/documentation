a47cfae878d60f9d656066d2c544dc3ddc7a8959
Banana <dana140198@gmail.com>
2018-10-19 19:22:05 +0600

    fix tab

--- tab/tab.js
+++ tab/tab.js
@@ -1,11 +1,16 @@
-document.querySelector('label[for="mac-radio"]').hover();
+
 function tabFunction(evt, osType) {
-    var i, tabs__content;
+    var i, tabs__content, tablabels;
     tabs__content = document.getElementsByClassName("tabs__content");
     for (i = 0; i < tabs__content.length; i++) {
         tabs__content[i].style.display = "none";
     }
+    
+   
     document.getElementById(osType).style.display = "block";
+
+
+    
 
 }   
 
